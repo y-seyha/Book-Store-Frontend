ccd9f6d5371e05dd96914653863a9d4205fffc1a
seyha yoeun <yoeunseyha11@gmail.com>
2026-04-05 22:48:53 +0700

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {ThemeProvider} from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+      <>
+        <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        </body>
+        </html>
+      </>
   );
 }
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default  function Profile (){
+
+    return (
+        <div className='min-h-screen flex justify-center items-center'>
+            <h2 className='font-bold text-3xl'>Profile Page</h2>
+        </div>
+    )
+}
