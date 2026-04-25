1b0c456f65a782a9d377a6638cd7ba879d98285d
seyha yoeun <yoeunseyha11@gmail.com>
2026-04-25 13:29:16 +0700

    Fix Cart Page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,16 +1,16 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+// import { cookies } from "next/headers";
+// import { redirect } from "next/navigation";
 import CartClient from "@/components/cart/CartClient";
 
 export default async function CartPage() {
-    const cookieStore = await cookies();
-
-    const token = cookieStore.get("access_token")?.value;
-    console.log(token);
-
-    if (!token) {
-        redirect("/auth/signin");
-    }
+    // const cookieStore = await cookies();
+    //
+    // const token = cookieStore.get("access_token")?.value;
+    // console.log(token);
+    //
+    // if (!token) {
+    //     redirect("/auth/signin");
+    // }
 
     return <CartClient />;
 }
